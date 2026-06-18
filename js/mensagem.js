a718ba7e80cac15655c9b33127f6047135fe4233
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-06-18 19:43:22 -0300

    Arquivo js/mensagem.js criado

---
+++ js/mensagem.js
@@ -0,0 +1,11 @@
+//padrão kenel case a partir da segunda palavra inicia letra maiuscula
+const mensagemTxtnum = (obj)=>{
+    return `${obj.nome}`
+}
+
+export {mensagemTxtnum}
+
+//se classe inicia maiuscula demais minuscula
+
+
+//
